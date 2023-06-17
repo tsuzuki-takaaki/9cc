edb703e4b5f14615ab471153800ddf91453ebe0c
alexanderpierce <takaaki.110706@gmail.com>
2023-06-17 14:18:06 +0000

    Create asm code generator based on ast

--- 9cc.c
+++ 9cc.c
@@ -5,6 +5,10 @@
 #include <stdlib.h>
 #include <string.h>
 
+// ----------
+// Tokenizer
+// ----------
+
 // Kinds of token as TokenKind
 typedef enum {
   TK_RESERVED, // for operator
@@ -12,9 +16,8 @@ typedef enum {
   TK_EOF, // for eof
 } TokenKind;
 
+// Type of token
 typedef struct Token Token;
-
-// Type of token struct
 struct Token {
   TokenKind kind; // kind of token(TokenKind)
   Token *next; // next token
@@ -51,7 +54,7 @@ void error_at(char *loc, char *fmt, ...) {
   exit(1);
 }
 
-// if next tokne is expected, read next token end return true.
+// if next token is expected, read next token end return true.
 // else return false.
 bool consume(char op) {
   if (token->kind != TK_RESERVED || token->str[0] != op)
@@ -112,7 +115,8 @@ Token *tokenize() {
       continue;
     }
 
-    if (*p == '+' || *p == '-') {
+    // for Punctuator
+    if (strchr("+-*/()", *p)) {
       cur = new_token(TK_RESERVED, cur, p++);
       continue;
     }
@@ -128,47 +132,165 @@ Token *tokenize() {
     // 	val: 5,
     // 	str: '5',
     // }
+    // for Integer Literal
     if (isdigit(*p)) {
       cur = new_token(TK_NUM, cur, p);
       cur->val = strtol(p, &p, 10);
       continue;
     }
-    error_at(p, "expected a number");
+    error_at(p, "invalid token");
   }
 
   new_token(TK_EOF, cur, p);
   return head.next;
 }
 
-int main(int argc, char **argv) {
-  if (argc != 2) {
-    fprintf(stderr, "invalid argument count\n");
-    return 1;
+// ----------
+// Parser
+// ----------
+
+// kinds of AST nodes
+typedef enum {
+  ND_ADD, // +
+  ND_SUB, // - 
+  ND_MUL, // *
+  ND_DIV, // /
+  ND_NUM, // int
+} NodeKind;
+
+// Type of AST node
+typedef struct Node Node;
+struct Node {
+  NodeKind kind; // node kind like ND_ADD
+  Node *lhs; // left side Node
+  Node *rhs; // right side Node
+  int val; // when kind is ND_NUM, use this member
+};
+
+// initialize Node
+Node *new_node(NodeKind kind) {
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = kind;
+  return node;
+}
+
+// For node that has lhs, rhs
+Node *new_binary(NodeKind kind, Node *lhs, Node *rhs) {
+  Node *node = new_node(kind);
+  node->lhs = lhs;
+  node->rhs = rhs;
+  return node;
+} 
+
+// For int ND_NUM
+Node *new_num(int val) {
+  Node *node = new_node(ND_NUM);
+  node->val = val;
+  return node;
+}
+
+// prototype
+Node *expr();
+Node *mul();
+Node *primary();
+
+// expr = mul ("+" mul | "-" mul)*
+Node *expr() {
+  Node *node = mul();
+  
+  for(;;) {
+    // consume: when token matches with argument, advance token
+    if (consume('+'))
+      node = new_binary(ND_ADD, node, mul());
+    else if (consume('-'))
+      node = new_binary(ND_SUB, node, mul());
+    else
+      return node;
   }
+}
+
+// mul = primary ("*" primary | "/" primary)*
+Node *mul() {
+  Node *node = primary();
+
+  for (;;) {
+    if (consume('*'))
+      node = new_binary(ND_MUL, node, primary());
+    else if (consume('/'))
+      node = new_binary(ND_DIV, node, primary());
+    else
+      return node;
+  }
+}
+
+// primary = num | "(" expr ")"
+Node *primary() {
+  // when token is (, next is maybe expr
+  if (consume('(')) {
+    Node *node = expr();
+    expect(')');
+    return node;
+  }
+
+  return new_num(expect_number());
+}
+
+// ----------
+// Code generator
+// ----------
+
+// receive AST and return asm output
+void gen(Node *node) {
+  if (node->kind == ND_NUM) {
+    printf("  push %d\n", node->val);
+    return;
+  }
+
+  gen(node->lhs);
+  gen(node->rhs);
+
+  printf("  pop rdi\n");
+  printf("  pop rax\n");
+
+  switch (node->kind) {
+  case ND_ADD:
+    printf("  add rax, rdi\n");
+    break;
+  case ND_SUB:
+    printf("  sub rax, rdi\n");
+    break;
+  case ND_MUL:
+    printf("  imul rax, rdi\n");
+    break;
+  case ND_DIV:
+    printf("  cqo\n");
+    printf("  idiv rdi\n");
+    break;
+  }
+
+  printf("  push rax\n");
+}
+
+int main(int argc, char **argv) {
+  if (argc != 2)
+    error("%s: invalid number of arguments", argv[0]);
 
   // create sequece of tokens
   user_input = argv[1];
   token = tokenize();
+  // create AST
+  Node *node = expr();
   
   // prepare of output assembly
   printf(".intel_syntax noprefix\n");
   printf(".globl main\n");
   printf("main:\n");
 
-  // first token is integer, so check the integer and create mov instruction
-  printf("  mov rax, %d\n", expect_number());
+  // generate asm code based on AST
+  gen(node);
 
-  // read tokens until eof token
-  while (!at_eof()) {
-    if (consume('+')) {
-      printf("  add rax, %d\n", expect_number());
-      continue;
-    }
-
-    expect('-');
-    printf("  sub rax, %d\n", expect_number());
-  }
-
+  // pop to finish
+  printf("  pop rax\n");
   printf("  ret\n");
   return 0;
 }
